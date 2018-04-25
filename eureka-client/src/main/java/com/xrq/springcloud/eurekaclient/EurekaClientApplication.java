6ede8f4bc54a3086539f31f94991f96b31178772
rqxiao <1007689622@qq.com>
2018-04-25 10:39:34 +0800

    删除 spring-cloud-netflix-eureka-client

--- eureka-client/src/main/java/com/xrq/springcloud/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/xrq/springcloud/eurekaclient/EurekaClientApplication.java
@@ -2,7 +2,6 @@ package com.xrq.springcloud.eurekaclient;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
