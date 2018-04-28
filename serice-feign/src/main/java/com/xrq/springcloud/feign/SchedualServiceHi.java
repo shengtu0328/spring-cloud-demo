15fd6cf7af61f165602afc2aa02fa4835391f1c9
rqxiao <1007689622@qq.com>
2018-04-28 12:04:39 +0800

    fegin

---
+++ serice-feign/src/main/java/com/xrq/springcloud/feign/SchedualServiceHi.java
@@ -0,0 +1,13 @@
+package com.xrq.springcloud.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+@Service
+@FeignClient(value = "service-hi")
+public interface SchedualServiceHi {
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
