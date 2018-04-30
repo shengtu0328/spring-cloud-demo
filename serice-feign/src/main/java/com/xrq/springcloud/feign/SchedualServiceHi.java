c50d70b98eb88a1b6154d38a8129cc88831188b3
shengtu0328 <1007689622@qq.com>
2018-04-30 13:36:35 +0800

    Hystrix

--- serice-feign/src/main/java/com/xrq/springcloud/feign/SchedualServiceHi.java
+++ serice-feign/src/main/java/com/xrq/springcloud/feign/SchedualServiceHi.java
@@ -1,12 +1,13 @@
 package com.xrq.springcloud.feign;
 
+import com.xrq.springcloud.feign.hystric.SchedualServiceHiHystric;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 @Service
-@FeignClient(value = "service-hi")
+@FeignClient(value = "service-hi",fallback = SchedualServiceHiHystric.class)
 public interface SchedualServiceHi {
     @RequestMapping(value = "/hi",method = RequestMethod.GET)
     String sayHiFromClientOne(@RequestParam(value = "name") String name);
